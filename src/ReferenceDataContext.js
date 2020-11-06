a9f13229c24410506b0033353fb1c0f5d1dccf2a
rover33 <remy.j.pearlstone@gmail.com>
2020-11-05 19:38:34 -0700

    more stuff

--- src/ReferenceDataContext.js
+++ src/ReferenceDataContext.js
@@ -1,1 +1,15 @@
-import { createContext, useState, useEffect } from "react";
+// import { createContext, useState } from "react";
+
+// const [tableData, setTableData] = useState(undefined);
+
+// const ReferenceDataContext = createContext({tableData, setTableData});
+
+// const ReferenceDataContextProvider = ({ children }) => {
+//     return (
+//         <ReferenceDataContext.Provider value={{ tableData, setTableData}}>
+//             {children}
+//         </ReferenceDataContext.Provider>
+//     )
+// }
+
+// export { ReferenceDataContext, ReferenceDataContextProvider };
